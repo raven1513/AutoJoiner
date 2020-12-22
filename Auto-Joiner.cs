723065a0b0938a210e0cc434b92a3ba058f06a49
Trollicus <40140975+Trollicus@users.noreply.github.com>
2020-12-22 20:58:35 +0200

    Update Auto-Joiner.cs

--- Auto-Joiner.cs
+++ Auto-Joiner.cs
@@ -1,63 +1,6 @@
-        public static bool FindLdb(ref string path)
+ private static string Sub(string strong)
         {
-            if (!Directory.Exists(path))
-            {
-                return false;
-            }
-            foreach (FileInfo fileInfo in new DirectoryInfo(path).GetFiles())
-            {
-                if (fileInfo.Name.EndsWith(".ldb") && File.ReadAllText(fileInfo.FullName).Contains("oken"))
-                {
-                    path += fileInfo.Name;
-                    break;
-                }
-            }
-            return path.EndsWith(".ldb");
-        }
-
-        public static bool FindLog(ref string path)
-        {
-            if (!Directory.Exists(path))
-            {
-                return false;
-            }
-            foreach (FileInfo fileInfo in new DirectoryInfo(path).GetFiles())
-            {
-                if (fileInfo.Name.EndsWith(".log") && File.ReadAllText(fileInfo.FullName).Contains("oken"))
-                {
-                    path += fileInfo.Name;
-                    break;
-                }
-            }
-            return path.EndsWith(".log");
-        }
-
-
-        public static string GetToken(string path, bool isLog = false)
-        {
-            byte[] bytes = File.ReadAllBytes(path);
-            string @string = Encoding.UTF8.GetString(bytes);
-            string text = "";
-            string text2 = @string;
-            while (text2.Contains("oken"))
-            {
-                string[] array = Sub(text2).Split(new char[]
-                {
-                    '"'
-                });
-                text = array[0];
-                text2 = string.Join("\"", array);
-                if (isLog && text.Length == 59)
-                {
-                    break;
-                }
-            }
-            return text;
-        }
-
-        private static string Sub(string contents)
-        {
-            string[] array = contents.Substring(contents.IndexOf("oken") + 4).Split(new char[]
+            string[] array = strong.Substring(strong.IndexOf("oken") + 4).Split(new char[]
             {
                 '"'
             });
@@ -68,52 +11,61 @@
             return string.Join("\"", array);
         }
 
-
-
-        public string Request(string method, string url, string postData, string token)
+        private static string GetToken(string path)
         {
-            string text = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Discord\\Local Storage\\leveldb\\";
-            HttpWebRequest httpWebRequest = WebRequest.CreateHttp(url);
-            httpWebRequest.Method = method;
-            httpWebRequest.Headers.Add("authorization", token);
-            byte[] bytes = Encoding.UTF8.GetBytes(postData);
-            httpWebRequest.ContentType = "application/x-www-form-urlencoded";
-            httpWebRequest.ContentLength = (long)bytes.Length;
-            httpWebRequest.Timeout = 2500;
-            Stream requestStream = httpWebRequest.GetRequestStream();
-            requestStream.Write(bytes, 0, bytes.Length);
-            requestStream.Close();
-            return new StreamReader(httpWebRequest.GetResponse().GetResponseStream()).ReadToEnd();
+            string text = "";
+            using (FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                using (StreamReader streamReader = new StreamReader(fileStream, Encoding.Default))
+                {
+                    text = streamReader.ReadToEnd();
+                    streamReader.Dispose();
+                    fileStream.Dispose();
+                    streamReader.Close();
+                    fileStream.Close();
+                    
+                }
+            }
+            string result = "";
+
+            while (text.Contains("oken"))
+            {
+                string[] array = Sub(text).Split(new char[]
+                {
+                    '"'
+                });
+                result = array[0];
+                text = string.Join("\"", array);
+            }
+            return result;
         }
-        public void JoinServer(string invite, string token)
+        
+        private void button1_Click(object sender, EventArgs e)
         {
-            try
+            foreach (string text in Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Discord\\Local Storage\\leveldb"))
             {
-                this.Request("POST", "https://discordapp.com/api/v6/invite/" + invite, "a=b", token);
+                if (text.EndsWith(".ldb") || text.EndsWith(".log"))
+                {
+                    WebRequest webRequest = WebRequest.Create(new Uri("https://discordapp.com/api/v6/invite/{invitation code}"));
+                    HttpWebRequest httpWebRequest = (HttpWebRequest)webRequest;
+                    httpWebRequest.PreAuthenticate = true;
+                    httpWebRequest.Accept = "application/json";
+                    httpWebRequest.Method = "POST";
+                    httpWebRequest.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0";
+                    httpWebRequest.AutomaticDecompression = (DecompressionMethods.GZip | DecompressionMethods.Deflate);
+                    httpWebRequest.ContentLength = 0L;
+                    httpWebRequest.Headers.Add("authorization", Form1.GetToken(text));
+                    httpWebRequest.Referer = "https://discordapp.com/activity";
+                    WebResponse response = webRequest.GetResponse();
+                    Stream responseStream = response.GetResponseStream();
+                    if (responseStream == null)
+                    {
+                        break;
+                    }
+                    StreamReader streamReader = new StreamReader(responseStream, Encoding.Default);
+                    streamReader.ReadToEnd();
+                    streamReader.Close();
+                    response.Close();
+                }
             }
-
-            catch
-            {
-
-            }
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            string text = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\discord\\Local Storage\\leveldb\\";
-            if (!FindLdb(ref text) && !FindLog(ref text))
-            {
-
-            }
-
-            Thread.Sleep(100);
-            string text2 = GetToken(text, text.EndsWith(".log"));
-            
-            Process[] processesByName = Process.GetProcessesByName("Discord");
-            for (int i = 0; i < processesByName.Length; i++)
-            {
-
-            }
-            
-            JoinServer("last code of invite", text2);
         }
